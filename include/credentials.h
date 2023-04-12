be7f4e204f5cf7eb89d51ed4d94dbf95e80b2bd9
agirilovich <agirilovich@gmail.com>
2023-04-12 18:24:32 +0200

    board replacement

---
+++ include/credentials.h
@@ -0,0 +1,11 @@
+#ifndef SECRETS
+#define SECRETS
+
+const char *ssid_name = "girilovich";
+const char *ssid_password = "1Dbiy tdfz nhfdf 4";
+
+const char *mqtt_server = "ha.girilowicz.lan";
+const char *mqtt_username = "well";
+const char *mqtt_password = "87zcndZdf5";
+
+#endif
