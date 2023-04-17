a2da727036e6eef8d4a6454ce747c638cbfd89ae
agirilovich <agirilovich@gmail.com>
2023-04-17 19:24:10 +0200

    optimisation of mqtt client

--- src/MQTT_task.cpp
+++ src/MQTT_task.cpp
@@ -1,11 +1,11 @@
 #include "MQTT_task.h"
 
-void initializeMQTTTopic(MQTTPubSubClient mqtt, const char *mqtt_user, const char *mqtt_pass, const char *StateTopic, const char* ConfigTopic, const char *ConfigMessage)
+void initializeMQTTTopic(MQTTPubSubClient mqtt, const char *mqtt_user, const char *mqtt_pass, const char *Topic, const String ConfigMessage)
 {
 
   Serial.print("Testing connection to mqtt broker...");
 
-  while (!mqtt.connect("stm32_well", mqtt_user, mqtt_pass))
+  while (!mqtt.connect(DEVICE_BOARD_NAME, mqtt_user, mqtt_pass))
   {
     Serial.print(".");
     delay(1000);
@@ -14,7 +14,7 @@ void initializeMQTTTopic(MQTTPubSubClient mqtt, const char *mqtt_user, const cha
   Serial.println(" connected!");
 
   //Publish message to AutoDiscovery topic
-  mqtt.publish(ConfigTopic, ConfigMessage, false, 0);
+  mqtt.publish(String(Topic) + "/config", ConfigMessage, false, 0);
   
   //Gracefully close connection to MQTT broker
   mqtt.disconnect();
@@ -22,10 +22,10 @@ void initializeMQTTTopic(MQTTPubSubClient mqtt, const char *mqtt_user, const cha
 }
 
 
-void publishMQTTPayload(MQTTPubSubClient mqtt, const char *mqtt_user, const char *mqtt_pass, const char *StateTopic, unsigned int PayloadMessage)
+void publishMQTTPayload(MQTTPubSubClient mqtt, const char *mqtt_user, const char *mqtt_pass, const char *Topic, unsigned int PayloadMessage)
 {
-  mqtt.connect("stm32_well", mqtt_user, mqtt_pass);
+  mqtt.connect(DEVICE_BOARD_NAME, mqtt_user, mqtt_pass);
   //mqtt.update();
-  mqtt.publish(StateTopic, String(PayloadMessage), false, 0);
+  mqtt.publish(String(Topic) + "/state", String(PayloadMessage), false, 0);
   mqtt.disconnect();
 }
